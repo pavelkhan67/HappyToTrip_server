099b60c323a609561bf0dd27bce75fdd8a24eab3
Pavel Khan <pvelkhantopu@gmail.com>
2023-07-12 20:11:42 +0600

    JWT token expire time extend

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function run() {
 
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '6h' })
             res.send({ token })
         })
 
